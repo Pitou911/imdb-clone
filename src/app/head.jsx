7ac62e18f23d07fd884f711ad6b116e79e8db67f
Pitou911 <songpitou723@gmail.com>
2023-04-22 16:14:27 -0500

    install nextjs

---
+++ src/app/head.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Head() {
+  return (
+    <>
+      <title>IMDB clone</title>
+      <meta content='width=device-width, initial-scale=1' name='viewport' />
+      <meta name='description' content='This is the imdb clone' />
+      <link rel='icon' href='/favicon.ico' />
+    </>
+  );
+}
---
+++ src/app/layout.jsx
@@ -0,0 +1,11 @@
+import "./globals.css";
+import Head from "./head";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang='en'>
+      <Head />
+      <body>{children}</body>
+    </html>
+  );
+}
