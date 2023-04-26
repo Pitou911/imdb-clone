1b577868d65fdc16b7eaa0f47d89f4abfbac475a
Pitou911 <songpitou723@gmail.com>
2023-04-25 21:48:32 -0500

    fetch data from tmdb api in the home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,23 @@
-export default function Home() {
-  return <h1 className='text-red-400'>Home</h1>;
+import Results from "@/components/Results";
+
+const API_KEY = process.env.API_KEY;
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 1000 } }
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const data = await res.json();
+  const results = data.results;
+
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
